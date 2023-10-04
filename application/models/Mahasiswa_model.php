b0f72cbc953531d810abd862cc058b7ec5cce397
aarais <114375673+aarais@users.noreply.github.com>
2023-10-04 21:29:28 +0700

    Membuat Class Mahasiswa

--- application/models/Mahasiswa_model.php
+++ application/models/Mahasiswa_model.php
@@ -1,1 +1,14 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
 
+class Mahasiswa_model extends CI_Model
+{
+    private $_table = "user_indra";
+
+    public $id_indra;
+    public $nama_indra;
+    public $username_indra;
+    public $password_indra;
+    public $level_indra;
+    //public $Nohp_139;
+    //    public $image = "default.jpg";
+    //    public $description;
